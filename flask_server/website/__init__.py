b8c43a4f1339e760af843f34de788b6e8ad86d05
Gaston Paz <gastipaz@gmail.com>
2022-11-05 12:23:28 +0100

    modified db postgres uri

--- flask_server/website/__init__.py
+++ flask_server/website/__init__.py
@@ -14,7 +14,7 @@ def create_app():
     CORS(app)
     login_manager = LoginManager()
     # app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
-    app.config['SQLALCHEMY_DATABASE_URI']="postgres://vyxbleyfeiiwje:607d4a0af85e726c0a9eab131ab5aecb706662ae33b4d67fb66e14e26b260edc@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/d5mftu97vfn67g"
+    app.config['SQLALCHEMY_DATABASE_URI']="postgresql://vyxbleyfeiiwje:607d4a0af85e726c0a9eab131ab5aecb706662ae33b4d67fb66e14e26b260edc@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/d5mftu97vfn67g"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
     db.init_app(app)
     Migrate(app, db)
